7f83b015b47fa68c9a0386e9d67ed935d26993e9
mraniki <mraniki@users.noreply.github.com>
2023-04-24 22:31:56 +0200

    fix: Refactor imports for findmyorder and fmosettings modules

--- examples/example.py
+++ examples/example.py
@@ -9,8 +9,7 @@ logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 logger.info(msg=f"log from settings: {settings.loglevel}")
 
-from findmyorder import findmyorder
-from findmyorder.config import fmosettings
+from findmyorder import findmyorder, fmosettings
 
 async def main():
     while True:
