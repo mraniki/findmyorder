a199e77ad4e3f1506702f4a36f77a8a001f7ea4d
mraniki <mraniki@users.noreply.github.com>
2023-04-25 08:29:21 +0200

    fix: Remove unnecessary validators from config.py file.

--- examples/config.py
+++ examples/config.py
@@ -6,8 +6,4 @@ settings = Dynaconf(
     settings_files=['findmyorder/default_settings.toml','settings.toml', '.secrets.toml'],
     load_dotenv=True,
     environments=True,
-    default_env="default",
-    validators=[
-        Validator("loglevel", default="INFO", apply_default_on_none=True),
-        ]
 )
