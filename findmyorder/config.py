e6362983c849c191fb2514e15695eaf46ff87d7d
mraniki <8766259+mraniki@users.noreply.github.com>
2023-04-25 17:20:01 +0200

    fix: default settings

--- findmyorder/config.py
+++ findmyorder/config.py
@@ -3,12 +3,12 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="FMO",
-    settings_files=['findmyorder/settings.toml', '.secrets.toml'],
+    settings_files=['settings.toml', '.secrets.toml'],
     load_dotenv=True,
     environments=True,
     default_env="default",
-    # validators=[
-    #     Validator("loglevel", default="INFO", apply_default_on_none=True),
-    #     #Validator("identifier", default=["BUY", "SELL", "buy", "sell","Buy","Sell"],apply_default_on_none=True),
-    #     ]
+    validators=[
+        Validator("loglevel", default="INFO", apply_default_on_none=True),
+        Validator("identifier", default=["BUY", "SELL", "buy", "sell","Buy","Sell"],apply_default_on_none=True),
+        ]
 )
