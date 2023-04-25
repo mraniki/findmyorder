1ca084137fcfae28e9f11f1f22c4b2a132d2d4a6
mraniki <8766259+mraniki@users.noreply.github.com>
2023-04-25 07:39:01 +0200

    fix: multiple config layer

--- examples/config.py
+++ examples/config.py
@@ -3,7 +3,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="TEST",
-    settings_files=['settings.toml', '.secrets.toml'],
+    settings_files=['findmyorder/default_settings.toml','settings.toml', '.secrets.toml'],
     load_dotenv=True,
     environments=True,
     default_env="default",
