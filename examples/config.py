a698c52898cc9879d3be9d6621e695873ff057dd
mraniki <mraniki@users.noreply.github.com>
2023-04-25 18:46:19 +0200

    fix: Remove config.py and settings.toml, comment out config import in example.py.

--- examples/config.py
+++
@@ -1,9 +0,0 @@
-
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    envvar_prefix="TEST",
-    settings_files=['settings.toml', '.secrets.toml'],
-    load_dotenv=True,
-    environments=True,
-)
