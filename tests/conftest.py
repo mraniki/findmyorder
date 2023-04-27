a0a8f321af11a26472ea2ef44d425347c0fb6689
mraniki <8766259+mraniki@users.noreply.github.com>
2023-04-27 23:01:25 +0200

    Delete conftest.py

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-# from dynaconf import settings
-
-
-# @pytest.fixture(scope="session", autouse=True)
-# def set_test_settings():
-#     settings.configure(FORCE_ENV_FOR_DYNACONF="findmyorder")
