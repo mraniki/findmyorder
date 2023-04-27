da50193c7696878692807ce234a35cbdcca9f2ca
mraniki <mraniki@users.noreply.github.com>
2023-04-27 20:59:16 +0200

    fix: import cleanup

--- findmyorder/main.py
+++ findmyorder/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from findmyorder.config import settings
 
-from pyparsing import one_of, Word, alphas, Regex, Optional
+from pyparsing import Regex, Optional
 
 
 class findmyorder:
