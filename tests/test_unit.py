c8884839b72840f65d94efc4471cddad02d81e68
mraniki <mraniki@users.noreply.github.com>
2023-04-27 23:13:52 +0200

    Refactor pytest version and add filterwarnings to pytest.ini

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,10 +2,6 @@
 from findmyorder import findmyorder
 
 
-# def test_dynaconf_is_in_testing_env():
-#     assert return_a_value() == "On Testing"
-
-
 def test_search():
     fmo = findmyorder()
     print(fmo)
