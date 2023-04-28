05a7f9939b63bdcdc4fe102e7f7fa29162832754
mraniki <8766259+mraniki@users.noreply.github.com>
2023-04-28 22:22:47 +0200

    fix: updated example get_order and remove regex logic

--- findmyorder/main.py
+++ findmyorder/main.py
@@ -10,30 +10,29 @@ class findmyorder:
                  ):
         self.logger =  logging.getLogger(__name__)
 
-    def search(self,message_to_parse: str = None,):
+    def search(self,mystring: str = None,):
       try:
-      #  print(settings.identifier)
-        myDict = settings.identifier
-
-        for word in myDict:
-            #self.logger.debug(f"Loop check {word}")
-            if re.search(word, message_to_parse):
-              self.logger.debug(f"found {word} in {message_to_parse}")
-              return True
-        self.logger.debug(f"no word identified in {message_to_parse}")
+        self.logger.debug(f"search for {mystring}")
+        action = one_of(settings.action_identifier, caseless=True).set_results_name("action")
+        order_grammar = action('action')
+        order = order_grammar.parse_string(instring=mystring,parse_all=False)
+          if order:
+            self.logger.debug(f"found {order} in {mystring}")
+            return True
+        self.logger.debug(f"no order identified in {mystring} using {settings.action_identifier}")
         return False
       except Exception as e:
-        self.logger.debug(f"error search {e}")
+        self.logger.debug(f"error search {mystring} {e}")
         return False
 
     def identify_order(self,mystring: str = None,):
       self.logger.debug(f"identify_order for {mystring}")
       try:
-        action = one_of("SELL BUY long short", caseless=True).set_results_name("action")
+        action = one_of(settings.action_identifier, caseless=True).set_results_name("action")
         instrument = Word(alphas).set_results_name("instrument")
-        stop_loss = Combine("sl=" + Word(nums)).set_results_name("stop_loss")
+        stop_loss = (PrecededBy(Literal("sl=")) & Word(nums)).set_results_name("stop_loss")
         take_profit = Combine("tp=" + Word(nums)).set_results_name("take_profit")
-        quantity = Combine("q=" + Word(nums)).set_results_name("quantity")
+        quantity = Combine("q=" + Word(nums)).set_results_name("quantity")+ Suppress("q=")
         ordertype = one_of(settings.order_type, caseless=True).set_results_name("ordertype")
         leverage_type = one_of(settings.leverage_type, caseless=True).set_results_name("leverage_type")
 
@@ -56,16 +55,12 @@ class findmyorder:
             self.logger.info(msg=f"get_order found: {mystring}")
             parsed_order = self.identify_order(mystring)
             self.logger.info(msg=f"get_order parsed_order: {parsed_order}")
-
-            # order_raw = mystring.split()
-            # self.logger.info(msg=f"Order identified: {order_raw}")
-
             order = {}
-            order['action'] = 'BUY'
-            order['instrument'] = 'EURUSD'
-            order['stoploss'] = 1000
-            order['takeprofit'] = {'tp1':10}
-            order['quantity'] = 10
+            order['action'] = parsed_order['action']
+            order['instrument'] =  parsed_order['instrument']
+            order['stoploss'] = parsed_order['takeprofit']
+            order['takeprofit'] = parsed_order['takeprofit']
+            order['quantity'] = parsed_order['quantity']
             order['timestamp'] = datetime.now(timezone.utc)
             # order['comments'] = 'findmyorder'
             # order['ordertype'] = 'spot'
