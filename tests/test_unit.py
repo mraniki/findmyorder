5b9e59f5e7485c0fd49fde8b1795976822870a6b
mraniki <mraniki@users.noreply.github.com>
2023-05-01 15:07:14 +0200

    fix: unit test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -8,16 +8,32 @@ from datetime import datetime, timezone
 from findmyorder import FindMyOrder as fmo
 
 
-async def test_search():
-    """Search Testing"""
-    assert await fmo.search("hello") is False
-    assert await fmo.search("buy btc") is True
-    assert await fmo.search("SELL BTC 1%") is True
-    assert await fmo.search("BUY BTCUSDT 1%") is True
-    assert await fmo.search("buy EURUSD sl=1000 tp=1000 q=1 comment=FOMC") is True
-    assert await fmo.search("sell EURGBP sl=200 tp=400 q=2%") is True
-    assert await fmo.search("LONG ETHUSD sl=200 tp=400 q=2%") is True
-    assert await fmo.search("SHORT CRUDEOIL sl=200 tp=400 q=2%") is True
+@pytest.fixture
+def find_my_order():
+    return FindMyOrder()
+
+def test_search_order(find_my_order):
+    mystring = "buy btc"
+    assert find_my_order.search(mystring) == True
+
+def test_search_no_order(find_my_order):
+    mystring = "This is not an order"
+    assert find_my_order.search(mystring) == False
+
+def test_search_exception(find_my_order, caplog):
+    mystring = ""
+    find_my_order.search(mystring)
+    assert "SearchError" in caplog.text
+# async def test_search():
+#     """Search Testing"""
+#     assert await fmo.search("hello") is False
+#     assert await fmo.search("buy btc") is True
+#     assert await fmo.search("SELL BTC 1%") is True
+#     assert await fmo.search("BUY BTCUSDT 1%") is True
+#     assert await fmo.search("buy EURUSD sl=1000 tp=1000 q=1 comment=FOMC") is True
+#     assert await fmo.search("sell EURGBP sl=200 tp=400 q=2%") is True
+#     assert await fmo.search("LONG ETHUSD sl=200 tp=400 q=2%") is True
+#     assert await fmo.search("SHORT CRUDEOIL sl=200 tp=400 q=2%") is True
 
 async def test_identify_order():
     """Identify Testing"""
