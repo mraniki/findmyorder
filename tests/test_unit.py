8d0a5e4005caaaa2d622f0c429c1112763706c2f
mraniki <8766259+mraniki@users.noreply.github.com>
2023-05-01 15:09:22 +0200

    Update tests/test_unit.py
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -18,7 +18,7 @@ def test_search_order(find_my_order):
 
 def test_search_no_order(find_my_order):
     mystring = "This is not an order"
-    assert find_my_order.search(mystring) == False
+    assert find_my_order.search(mystring) is False
 
 def test_search_exception(find_my_order, caplog):
     mystring = ""
