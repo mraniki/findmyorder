59555b20c835071c8f16dacd037907b105859ac4
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-05 15:54:53 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,70 +1,61 @@
-# Configuration file for the Sphinx documentation builder.
-
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+import datetime
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../src"))
+from findmyorder import __version__
 
-# -- Project information -----------------------------------------------------
+sys.path.append(os.path.abspath(os.pardir))
 
-project = "{{project_name}}"
-copyright = "2023, {{author_name}}"
-author = "{{author_name}}"
+# -- General configuration ----------------------------------------------------
+templates_path = ['_templates']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.ifconfig',
+              'sphinx.ext.extlinks']
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'Pelican'
+year = datetime.datetime.now().date().year
+copyright = f'2023–{year}'
+exclude_patterns = ['_build']
+release = __version__
+version = '.'.join(release.split('.')[:1])
+last_stable = __version__
+rst_prolog = '''
+.. |last_stable| replace:: :findmyorder-doc:`{}`
+'''.format(last_stable)
+
+extlinks = {
+    'pelican-doc':  ('https://talkytrader.gitbook.io/talky/', '%s')
+}
+
+# -- Options for HTML output --------------------------------------------------
+
+html_theme = 'furo'
+html_title = f'<strong>{project}</strong> <i>{release}</i>'
+html_static_path = ['_static']
+html_theme_options = {
+    'light_logo': 'talky-logo.svg',
+    'dark_logo': 'talky-logo.svg',
+    'navigation_with_keys': True,
+}
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'Pelicandoc'
+
+html_use_smartypants = True
+
+# If false, no module index is generated.
+html_use_modindex = False
+
+# If false, no index is generated.
+html_use_index = False
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = False
 
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.viewcode",
-    "myst_parser",
-    "autodoc2",
-]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+def setup(app):
+    # overrides for wide tables in RTD theme
+    app.add_css_file('theme_overrides.css')   # path relative to _static
 
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-
-autodoc2_packages = [
-    "../../src/{{package_name}}",
-]
-# Enable all docstrings as Myst Markdown
-autodoc2_docstring_parser_regexes = [
-    (r".*", "myst"),
-]
-
-myst_heading_anchors = 2
