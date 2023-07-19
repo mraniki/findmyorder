2916a03bec3579a1e7568984aa99eb8ca0c13d9b
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-19 17:02:22 +0200

    ✅ unit test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -218,19 +218,3 @@ async def test_contains_emoji(fmo,order_with_emoji):
     result = await fmo.contains_emoji(order_with_emoji)
     assert result is True
 
-
-@pytest.mark.asyncio
-async def test_exception_handling():
-    # Simulate an exception
-    def func_that_raises():
-        raise ValueError("Test error")
-
-    # Call the function and verify that it returns None
-    result = None
-    try:
-        result = func_that_raises()
-    except Exception:
-        result = None
-
-    # Check that the function returned None
-    assert result is None
