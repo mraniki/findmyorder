af2f61653355b864bc28bc2205f05bdebc890348
mraniki <8766259+mraniki@users.noreply.github.com>
2023-10-31 06:55:38 +0100

    ♻️ simplify example

--- examples/example.py
+++ examples/example.py
@@ -3,16 +3,8 @@ Provides example for FindMyOrder
 """
 
 import asyncio
-import sys
-
-import uvicorn
-from fastapi import FastAPI
-from loguru import logger
 
 from findmyorder import FindMyOrder
-
-logger.remove()
-logger.add(sys.stderr, level="INFO")
 
 
 async def main():
@@ -45,26 +37,5 @@ async def main():
         await asyncio.sleep(7200)
 
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-async def start():
-    """startup"""
-    asyncio.create_task(main())
-
-
-@app.get("/")
-def read_root():
-    """root"""
-    return {"FMO is online"}
-
-
-@app.get("/health")
-def health_check():
-    """healthcheck"""
-    return {"FMO is online"}
-
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    asyncio.run(main())
